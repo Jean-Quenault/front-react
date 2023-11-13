b43b79bb174c5588f048ebdd9ac758beb81f9858
Jean-Quenault <jeanform@proton.me>
2023-11-13 11:50:12 +0100

    Add .env, database content listing.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 
+// 
+const serverUrl = process.env.REACT_APP_SERVER_URL;
+
 function App() {
   const [ip, setIp] = useState('');
   const [os, setOs] = useState('');
@@ -22,12 +25,60 @@ function App() {
     setBrowser(navigator.userAgent);
   }, []);
 
+  // Add the user's informations to the database, by a POST request to the back
+
+  const handleSubmit = () => {
+    fetch(`${serverUrl}/users`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        ip: ip,
+        navigateurs: browser,
+        os: os,
+      }),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Succès:', data);
+      alert("Vos informations ont été ajoutées avec succès !");
+    })
+    .catch((error) => {
+      console.error('Erreur:', error);
+      alert("Une erreur est survenue lors de l'envoi de vos informations.");
+    });
+  };
+
+  // Show all informations from the database, by a GET request to the back
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(`${serverUrl}/users`)
+      .then(response => response.json())
+      .then(data => {
+        setUsers(data);
+      })
+      .catch(error => console.error('Erreur:', error));
+  }, []);
+  
+
   return (
     <div>
       <h1>Informations sur votre ordinateur</h1>
       <p>Adresse IP : {ip}</p>
       <p>Système d'exploitation : {os}</p>
       <p>Navigateur : {browser}</p>
+      <button onClick={handleSubmit}>Envoyer mes informations</button>
+      <h1>Liste des utilisateurs</h1>
+      <ul>
+        {users.map((user, index) => (
+          <li key={index}>
+            IP: {user.ip}, Navigateur: {user.navigateurs}, OS: {user.os}
+          </li>
+      ))}
+    </ul>
     </div>
   );
 }
